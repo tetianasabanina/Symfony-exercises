b6639f486dbcab793dde25dd93665dc8feaf2f91
tatiana_sabanina <tatiana08st@gmail.com>
2019-09-05 16:45:23 +0300

    Modified: koodinkoulu_testi

---
+++ src/Controller/EsimerkitController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+
+class EsimerkitController{
+    // Kontrollerit tulee tänne
+
+    public function laskePalkka(){
+        $nettopalkka = 4500 * 0.7;
+
+        return new Response('<h2>Bruttopalkkasi on 4500 ja nettopalkkasi on <strong>' . $nettopalkka . '</strong></h2>');
+
+    }
+
+    public function tarkistaKarkausvuosi(){
+        if (2019%4 == 0){
+            $vastaus = " on karkaus";
+        }else{
+            $vastaus = " ei ole karkaus";
+        }
+        return new Response('<h2>Vuosi '. 2019 . '<strong>' . $vastaus . ' vuosi</strong>' );
+    }
+
+    public function laskePH(){
+        $x = 2.13*pow(10,-5);
+        $ph = -log10($x);
+        return new Response('<h2>Kun vesiliuoksen vetyioni-konsenraatioon ' . $x . ' mol/l sen pH on <strong>' . $ph . '.</strong></h2>' );
+    }
+
+    public function heittaNoppa(){
+        $noppa = rand(1, 6);
+        return new Response('<h2>Nopan silmäluku on ' . $noppa . '.</strong></h2>' );
+    }
+
+    public function naytaJSON(){
+        // Henkilo-taulukko
+        $nimet = [
+            'Etunimi' => 'Pekka',
+            'Sukunimi' => 'Puupää',       
+        ];
+                
+        return new JsonResponse($nimet);
+    }
+
+    public function lihapiirakka(){
+        $rahat = 10;
+        $hinta = 2.5; 
+        if ($rahat > $hinta){
+            $vastaus = "Sinulla on rahamäärä " . round($rahat/$hinta) . ':n piirakkaan.';
+        } else {
+            $vastaus = "Sinun täytyy paastota";
+        }
+        return new Response('<h2>' . $vastaus . '</h2>');
+    }
+}
+
+
+
